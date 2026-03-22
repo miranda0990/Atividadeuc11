3d6303a82bf65728d1f5c1db866d4078ad901588
MIRANDA <miranda20090701@gmail.com>
2026-03-22 13:25:08 -0300

    Projeto inicial

---
+++ src/ProdutosDAO.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Adm
+ */
+
+import java.sql.PreparedStatement;
+import java.sql.Connection;
+import javax.swing.JOptionPane;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+
+public class ProdutosDAO {
+    
+    Connection conn;
+    PreparedStatement prep;
+    ResultSet resultset;
+    ArrayList<ProdutosDTO> listagem = new ArrayList<>();
+    
+    public int cadastrarProduto (ProdutosDTO produto){
+        
+      
+        
+    }
+    
+    public ArrayList<ProdutosDTO> listarProdutos(){
+     
+        
+        return listagem;
+    }
+    
+    
+        
+}
+
