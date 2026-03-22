fa05303395e437331c52b1e3a5114ab42aa7aaf3
MIRANDA <miranda20090701@gmail.com>
2026-03-22 13:34:03 -0300

    criação do metodo listarProdutos

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -45,7 +45,32 @@ public class ProdutosDAO {
     public ArrayList<ProdutosDTO> listarProdutos(){
      
         
+       String sql = "SELECT * FROM produtos"; 
+        
+        conn = new conectaDAO().connectDB();
+        
+        try {
+            prep = conn.prepareStatement(sql);
+            resultset = prep.executeQuery();
+            
+            while (resultset.next()) {
+                ProdutosDTO produto = new ProdutosDTO();
+                
+                
+                produto.setId(resultset.getInt("id"));
+                produto.setNome(resultset.getString("nome"));
+                produto.setValor(resultset.getInt("valor"));
+                produto.setStatus(resultset.getString("status"));
+                
+                listagem.add(produto);
+            }
+        } catch (SQLException erro) {
+            JOptionPane.showMessageDialog(null, "Erro ao listar produtos no DAO: " + erro.getMessage());
+        }
+        
         return listagem;
+
+
     }
     
     
